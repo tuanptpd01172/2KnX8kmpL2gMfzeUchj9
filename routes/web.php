632b009737f1e41fe0015ac23c11fc0f5291465e
tuanptpd01172 <phamthanhtuan1990.qn@gmail.com>
2018-02-22 16:57:50 +0700

    lang
    22/02/2018

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
-<?php
 
+<?php
+// use Session;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,10 +12,28 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*middeware lang*/
+Route::group(['middleware'=>'locale'],function (){
+	Route::get('/', function () {
+	    return view('welcome');
+	});
 
-Route::get('/public', function () {
-    return 1;
+	Route::get('/langs/en', function () {
+	    return view('admin.test');
+	});
+	Route::get('welcome/{locale?}', function ($locale = null) {
+	    
+	    return view('admin.test');
+	});
+	Route::get('/change-lang/{locale?}', function ($locale) {
+	    Session::put('locale',$locale);
+	    return Redirect::route('test');
+	    
+	   
+	});
+	Route::get('/test', function () {
+	    // App::setLocale('en');
+	    return view('admin.test');
+	   
+	})->name('test');
 });
---
+++ storage/framework/views/e92ccef10aab321d236af0c5402220b8b8c85f9d.php
@@ -0,0 +1,8 @@
+
+<?php echo e(__('messages.ss')); ?>
+
+<?php echo app('translator')->getFromJson('messages.ss'); ?>
+<?php
+	// echo __('ss');
+	echo __('messages.welcome');
+ ?>
